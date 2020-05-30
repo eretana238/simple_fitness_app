5a658b6b892971de22127911005a1019184d3e44
eretana238 <eretana238@gmail.com>
2020-05-29 22:09:49 -0600

    added header text

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -11,15 +11,66 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Color primaryColor = Color(0x58484599);
+  Color secondaryColor = Color.fromARGB(255, 85, 53, 145);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            child: ,
-          ),
-        ],
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(vertical: 20.0),
+          children: <Widget>[
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 25.0, vertical: 15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    FaIcon(
+                      FontAwesomeIcons.gripHorizontal,
+                      color: Color(0x58484599),
+                    ),
+                    Container(
+                      child: GestureDetector(
+                        onTap: () => print('pressed profile pic'),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(30.0),
+                          child: Image(
+                            image: AssetImage('assets/images/me.jpg'),
+                            height: 45.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 25.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'May 29, 2020',
+                    style: TextStyle(
+                      color: primaryColor,
+                    ),
+                  ),
+                  Text(
+                    'Have a nice day, Esteban!',
+                    style: TextStyle(
+                      color: secondaryColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30.0,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
